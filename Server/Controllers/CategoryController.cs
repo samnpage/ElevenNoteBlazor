f6cdaa412ef130c40881183f67d82d33c5ab8ed5
Sam Page <samnpage96@gmail.com>
2023-12-15 17:43:08 -0500

    initial commit

---
+++ Server/Controllers/CategoryController.cs
@@ -0,0 +1,45 @@
+using ElevenNoteBlazor.Server.Services.Category;
+using ElevenNoteBlazor.Shared.Category;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Server.Controllers;
+[ApiController]
+[Route("api/[controller]")]
+public class CategoryController : ControllerBase
+{
+    private readonly ICategoryService _categoryService;
+
+    public CategoryController(ICategoryService categoryService)
+    {
+        _categoryService = categoryService;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var categories = await _categoryService.GetAllCategoriesAsync();
+        return Ok(categories);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> Category(int id)
+    {
+        var category = await _categoryService.GetCategoryByIdAsync(id);
+        if (category == null)
+            return NotFound();
+
+        return Ok(category);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(CategoryCreate model)
+    {
+        if (model == null || !ModelState.IsValid)
+            return BadRequest();
+
+        bool wasSuccessful = await _categoryService.CreateCategoryAsync(model);
+        if (!wasSuccessful)
+            return UnprocessableEntity();
+
+        return Ok();
+    }
+}
